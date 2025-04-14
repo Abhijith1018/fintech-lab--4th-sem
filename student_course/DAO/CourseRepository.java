cfebdaa6c99d8475574f68fb3b1fed58e3abee92
Abhijith1018 <147328230+Abhijith1018@users.noreply.github.com>
2025-04-15 01:36:13 +0530

    Delete student_course directory

--- student_course/DAO/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exam.student_course.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.exam.student_course.Entity.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-    
-}
--- student_course/DAO/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exam.student_course.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.exam.student_course.Entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-    
-}
